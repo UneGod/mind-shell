db28467714a357a2429752a5e20ce21a9d698235
UneGod <klimok71@gmail.com>
2025-03-28 11:50:37 +0600

    added kostil of gamepc

--- scripts/interact.gd
+++ scripts/interact.gd
@@ -1,6 +1,8 @@
 extends Area3D
 
 @onready var interact = $"../Interaction"
+@onready var gamepc = $"../Gamepc"
+
 var entered = false
 
 # Called when the node enters the scene tree for the first time.
@@ -10,16 +12,20 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if entered:
+		if Input.is_action_just_pressed("interact"):
+				gamepc.show()
+				interact.hide()
 
 
 func _on_body_entered(body) -> void:
-	entered = true
 	if body.is_in_group("Player"):
+		entered = true
 		interact.show()
 
 
 func _on_body_exited(body) -> void:
-	entered = false
 	if body.is_in_group("Player"):
+		entered = false
+		gamepc.hide()
 		interact.hide()
