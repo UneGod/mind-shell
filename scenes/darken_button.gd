7eca11dcc33ac1b620e12e9788faea14bc1870db
UneGod <klimok71@gmail.com>
2025-03-31 10:27:27 +0600

    svet nkaramelev

--- scenes/darken_button.gd
+++
@@ -1,6 +0,0 @@
-extends Button
-
-@onready var darkener = $"../../Darkener"  # Путь от кнопки к Darkener
-
-func _ready():
-	connect("pressed", Callable(darkener, "toggle_darkness"))
--- scripts/mainscene.gd
+++ scripts/mainscene.gd
@@ -1,10 +1,12 @@
 extends Node3D
 
-# Убедитесь, что камера и затемнитель правильно инициализированы
-@onready var darkener = $Darkener
-@onready var camera = $Camera3D
+@onready var dark = $DirectionalLight3D
 
-func _ready():
-	# Настройка начальной позиции камеры (опционально)
-	camera.global_transform.origin = Vector3(0, 2, 5)
-	camera.look_at(Vector3.ZERO)
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _process(_delta) -> void:
+	if Input.is_action_just_pressed("darktheme"):
+		dark.light_color = Color(0, 0, 0, 13)
+	if Input.is_action_just_pressed("backdark"):
+		dark.light_color = Color(1, 1, 1)
